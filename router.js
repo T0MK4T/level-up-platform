7f8c4cbd477a7638fd2e3f9adc7b1a8581e10755
T0MK4T <thomasbeckrussell@gmail.com>
2018-10-22 23:00:50 -0500

    added node server structure

---
+++ router.js
@@ -0,0 +1,16 @@
+app.get('/posts', (req, res) => {
+  
+});
+
+app.get('/posts/:id', (req, res) => {
+  
+});
+
+app.post('/posts', (req, res) => {
+  
+  }
+
+
+app.delete('/posts/:id', (req, res) => {
+  
+});
